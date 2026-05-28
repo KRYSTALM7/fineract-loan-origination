c9fa67efbf07e204ae0a8e5dc187cab5544289ce
Sujan Kumar MV <sujankumar7702@gmail.com>
2026-05-28 23:11:42 +0530

    FINERACT-2442: fix gitattributes, gitignore, CI env vars and added spotless plugin

--- src/main/java/org/apache/fineract/los/LoanOriginationApplication.java
+++ src/main/java/org/apache/fineract/los/LoanOriginationApplication.java
@@ -24,7 +24,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoanOriginationApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LoanOriginationApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(LoanOriginationApplication.class, args);
+  }
 }
